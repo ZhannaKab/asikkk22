68c588ed8e1eb25c0024a4f70ebffdc34d885974
zhannakabdullina <kabdullina07@icloud.com>
2025-04-13 19:43:52 +0500

    com

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        // Проверка стека
+
         MyStack<Integer> stack = new MyStack<>();
         stack.push(10);
         stack.push(20);
         stack.push(30);
         System.out.println("Stack pop: " + stack.pop());
 
-        // Проверка очереди
         MyQueue<String> queue = new MyQueue<>();
         queue.enqueue("A");
         queue.enqueue("B");
         queue.enqueue("C");
         System.out.println("Queue dequeue: " + queue.dequeue());
 
-        // Проверка кучи
         MyMinHeap heap = new MyMinHeap();
         heap.add(5);
         heap.add(3);
